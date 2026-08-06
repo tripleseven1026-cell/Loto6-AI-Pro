3c428bf4d491b5347e1fedd5145090878072b2ef
tripleseven1026-cell <tripleseven1026@gmail.com>
2026-08-06 18:07:06 +0900

    Create prediction.js

---
+++ prediction.js
@@ -0,0 +1,12 @@
+// prediction.js
+// Loto6 AI Pro
+
+function createPrediction(history) {
+
+    if (!history || history.length === 0) {
+        return [];
+    }
+
+    // 仮の予測番号
+    return [1, 8, 14, 22, 31, 40];
+}
